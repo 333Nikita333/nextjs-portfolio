007d5d2673ede2389247089f52743eb3d9089374
333Nikita333 <nikitosfox333@gmail.com>
2023-10-02 23:04:53 +0300

    fix 2 pre-deployments

--- pages/about/index.js
+++ pages/about/index.js
@@ -23,19 +23,19 @@ const aboutData = [
     info: [
       {
         title: 'Web Development',
-        // icons: [
-          // <FaHtml5 />,
-          // <FaCss3 />,
-          // <FaJs />,
-          // <FaReact />,
-          // <SiNextdotjs />,
-          // <SiFramer />,
-          // <FaWordpress />,
-        // ],
+        icons: [
+          <FaHtml5 key="html5" />,
+          <FaCss3 key="css3" />,
+          <FaJs key="js" />,
+          <FaReact key="react" />,
+          <SiNextdotjs key="nextjs" />,
+          <SiFramer key="framer" />,
+          <FaWordpress key="wordpress" />,
+        ],
       },
       {
         title: 'UI/UX Design',
-        // icons: [<FaFigma />, <SiAdobexd />, <SiAdobephotoshop />],
+        icons: [<FaFigma key="figma" />, <SiAdobexd key="adobexd" />, <SiAdobephotoshop key="photoshop" />],
       },
     ],
   },
