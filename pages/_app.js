94b29ba754057761bb532982e421923f03de6760
333Nikita333 <nikitosfox333@gmail.com>
2023-10-03 23:17:37 +0300

    setting transition for pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,20 @@
+import { useRouter } from 'next/router';
 import Layout from '../components/Layout';
 import '../styles/globals.css';
-
-//components
+import { AnimatePresence, motion } from 'framer-motion';
+import Transition from '../components/Transition';
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
   return (
     <Layout>
-      <Component {...pageProps} />
+      <AnimatePresence mode="wait">
+        <motion.div key={router.route} className="h-full">
+          <Transition />
+          <Component {...pageProps} />
+        </motion.div>
+      </AnimatePresence>
     </Layout>
   );
 }
