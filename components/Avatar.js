fbfa66cbb32e2b062b3158c7ff7e5babfb4b3f88
333Nikita333 <nikitosfox333@gmail.com>
2023-10-04 23:08:51 +0300

    home page styling

--- components/Avatar.js
+++ components/Avatar.js
@@ -1,5 +1,17 @@
+import Image from 'next/image';
+
 const Avatar = () => {
-  return <div>Avatar</div>;
+  return (
+    <div className="hidden xl:flex xl:max-w-none">
+      <Image
+        src={'/avatar-image.png'}
+        width={642}
+        height={801}
+        alt=""
+        className="translate-z-0 w-full h-full"
+      />
+    </div>
+  );
 };
 
 export default Avatar;
--- components/ProjectsBtn.js
+++ components/ProjectsBtn.js
@@ -1,5 +1,26 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import { HiArrowRight } from 'react-icons/hi2';
+
 const ProjectsBtn = () => {
-  return <div>Project Btn</div>;
+  return (
+    <div className="mx-auto xl:mx-0">
+      <Link
+        href={'/work'}
+        className="relative w-[145px] h-[145px] xl:w-[165px] xl:h-[165px] flex justify-center items-center 
+        bg-circleStar bg-cover bg-center bg-no-repeat group"
+      >
+        <Image
+          src={'/rounded-text.png'}
+          width={141}
+          height={148}
+          alt=""
+          className="animate-spin-slow w-full h-full max-w-[101px] max-h-[108px] xl:max-w-[121px] xl:max-h-[128px]"
+        />
+        <HiArrowRight className="absolute text-4xl group-hover:translate-x-2 transition-all duration-300" />
+      </Link>
+    </div>
+  );
 };
 
 export default ProjectsBtn;
