463d0cfcba6d1ad5237691fcd64bd9d5af7a9e43
333Nikita333 <nikitosfox333@gmail.com>
2023-10-10 01:39:31 +0300

    finished styling the services page

--- components/ServiceSlider.js
+++ components/ServiceSlider.js
@@ -16,16 +16,35 @@ import {
   RxArrowTopRight,
 } from 'react-icons/rx';
 
-// service data
 export const serviceData = [
+  // {
+  //   icon: <RxCrop />,
+  //   title: 'Branding',
+  //   description: 'Lorem ipsum dolor sit anet, consectetur adipiscing alit.',
+  // },
+  // {
+  //   icon: <RxPencil2 />,
+  //   title: 'Design',
+  //   description: 'Lorem ipsum dolor sit anet, consectetur adipiscing alit.',
+  // },
+  // {
+  //   icon: <RxDesktop />,
+  //   title: 'Development',
+  //   description: 'Lorem ipsum dolor sit anet, consectetur adipiscing alit.',
+  // },
+  // {
+  //   icon: <RxReader />,
+  //   title: 'Copywriting',
+  //   description: 'Lorem ipsum dolor sit anet, consectetur adipiscing alit.',
+  // },
+  // {
+  //   icon: <RxRocket />,
+  //   title: 'SEO',
+  //   description: 'Lorem ipsum dolor sit anet, consectetur adipiscing alit.',
+  // },
   {
-    icon: <RxCrop />,
-    title: 'Branding',
-    description: 'Lorem ipsum dolor sit anet, consectetur adipiscing alit.',
-  },
-  {
-    icon: <RxPencil2 />,
-    title: 'Design',
+    icon: <RxDesktop />,
+    title: 'Development',
     description: 'Lorem ipsum dolor sit anet, consectetur adipiscing alit.',
   },
   {
@@ -34,13 +53,18 @@ export const serviceData = [
     description: 'Lorem ipsum dolor sit anet, consectetur adipiscing alit.',
   },
   {
-    icon: <RxReader />,
-    title: 'Copywriting',
+    icon: <RxDesktop />,
+    title: 'Development',
     description: 'Lorem ipsum dolor sit anet, consectetur adipiscing alit.',
   },
   {
-    icon: <RxRocket />,
-    title: 'SEO',
+    icon: <RxDesktop />,
+    title: 'Development',
+    description: 'Lorem ipsum dolor sit anet, consectetur adipiscing alit.',
+  },
+  {
+    icon: <RxDesktop />,
+    title: 'Development',
     description: 'Lorem ipsum dolor sit anet, consectetur adipiscing alit.',
   },
 ];
