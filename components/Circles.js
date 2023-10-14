e9583c05b0a97ebb8dbec6bc0f57539b9e52a5e4
333Nikita333 <nikitosfox333@gmail.com>
2023-10-14 23:16:27 +0300

    finished styling the contact page

--- components/Circles.js
+++ components/Circles.js
@@ -4,9 +4,15 @@ const Circles = () => {
   return (
     <div
       className="w=[200px] xl:w-[300px] absolute -right-16 -bottom-2 
-    mix-blend-color-dodge animate-pulse duration-75 z-1"
+    mix-blend-color-dodge animate-pulse duration-75 pointer-events-none"
     >
-      <Image src={'/circles.png'} width={260} height={200} alt="" className="w-full h-full" />
+      <Image
+        src={'/circles.png'}
+        width={260}
+        height={200}
+        alt=""
+        className="w-full h-full"
+      />
     </div>
   );
 };
