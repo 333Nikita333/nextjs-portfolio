91ebd894e7a66677657e2b00068758cac40d8478
Mac <andrej@MacBook-Pro-Andrej.local>
2023-12-08 23:48:01 +0200

    fix deploy

--- pages/projects/[id].js
+++ pages/projects/[id].js
@@ -137,30 +137,35 @@ export default function Project({ project }) {
     // technologies
     {
       title: 'Technologies',
-      content: project.technologies ? (
+      content: (
         <ul className="flex flex-wrap gap-10">
-          {Object.keys(project.technologies).map(techType => (
+          {['frontend', 'backend'].map(techType => (
             <li key={techType}>
-              <p>{techType[0].toUpperCase() + techType.slice(1)}:</p>
-              <ul className="flex flex-wrap gap-y-2 gap-x-1">
-                {project.technologies[techType] &&
-                project.technologies[techType].length > 0
-                  ? project.technologies[techType].sort().map((tech, index) => (
-                      <li key={index}>
-                        <span
-                          key={index}
-                          className="bg-gray-300 text-gray-800 rounded-full px-3 py-1 text-sm mr-2 mb-2"
-                        >
-                          {tech}
-                        </span>
-                      </li>
-                    ))
-                  : null}
-              </ul>
+              {project.technologies &&
+              project.technologies[techType] &&
+              project.technologies[techType].length > 0 ? (
+                <>
+                  <p>{techType[0].toUpperCase() + techType.slice(1)}:</p>
+                  <ul className="flex flex-wrap gap-y-2 gap-x-1">
+                    {project.technologies[techType]
+                      .sort()
+                      .map((tech, index) => (
+                        <li key={index}>
+                          <span
+                            key={index}
+                            className="bg-gray-300 text-gray-800 rounded-full px-3 py-1 text-sm mr-2 mb-2"
+                          >
+                            {tech}
+                          </span>
+                        </li>
+                      ))}
+                  </ul>
+                </>
+              ) : null}
             </li>
           ))}
         </ul>
-      ) : null,
+      ),
     },
     // features
     {
