42bb75a0acc209a7e938b413041ab81483b2458b
Mac <andrej@MacBook-Pro-Andrej.local>
2023-12-09 00:23:15 +0200

    fix deploy6

--- pages/api/data/projectsData.js
+++ pages/api/data/projectsData.js
@@ -349,7 +349,9 @@ export const projectsData = [
             href="https://connections-api.herokuapp.com/docs"
             target="_blank"
             rel="noreferrer nofollow noopener"
-          >link</a>
+          >
+            link
+          </a>
         )}`,
       },
       {
@@ -508,6 +510,7 @@ export const projectsData = [
         'Notiflix',
         'Tui-pagination',
       ],
+      backtend: null,
     },
   },
   // 2-Ice Cream
@@ -581,6 +584,7 @@ export const projectsData = [
         'Animate.css',
         'Modern-normalize',
       ],
+      backtend: null,
     },
   },
   // 1-Web Studio
