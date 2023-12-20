652256a2b676578e246ec72e7c5171f64fe72df4
333Nikita333 <nikitosfox333@gmail.com>
2023-12-20 12:34:46 +0200

    added my social links

--- components/Socials.js
+++ components/Socials.js
@@ -1,51 +1,46 @@
 import Link from 'next/link';
 import {
-  RiBehanceLine,
-  RiDribbbleLine,
-  RiFacebookBoxLine,
+  RiGithubFill,
+  RiLinkedinBoxFill,
+  RiTelegramFill,
   RiInstagramLine,
-  RiPinterestLine,
-  RiYoutubeLine,
 } from 'react-icons/ri';
+import { MdEmail } from 'react-icons/md';
 
 const socials = [
   {
-    path: '',
-    icon: <RiYoutubeLine size={25} />,
-    styles: 'hover:text-accent transition-all duration-300',
+    path: 'https://github.com/333Nikita333',
+    icon: <RiGithubFill size={25} />,
   },
   {
-    path: '',
-    icon: <RiFacebookBoxLine size={25} />,
-    styles: 'hover:text-accent transition-all duration-300',
+    path: 'mailto:mykytahilis@gmail.com',
+    icon: <MdEmail size={25} />,
   },
   {
-    path: '',
+    path: 'https://instagram.com/nik_gilis',
     icon: <RiInstagramLine size={25} />,
-    styles: 'hover:text-accent transition-all duration-300',
   },
   {
-    path: '',
-    icon: <RiDribbbleLine size={25} />,
-    styles: 'hover:text-accent transition-all duration-300',
+    path: 'https://linkedin.com/in/mykyta-hilis',
+    icon: <RiLinkedinBoxFill size={25} />,
   },
   {
-    path: '',
-    icon: <RiBehanceLine size={25} />,
-    styles: 'hover:text-accent transition-all duration-300',
-  },
-  {
-    path: '',
-    icon: <RiPinterestLine size={25} />,
-    styles: 'hover:text-accent transition-all duration-300',
+    path: 'https://t.me/NikitaGilis',
+    icon: <RiTelegramFill size={25} />,
   },
 ];
 
 const Socials = () => {
   return (
     <div className="flex items-center gap-x-5 text-lg">
-      {socials.map(({ path, icon, styles }, index) => (
-        <Link key={index} href={path} className={styles}>
+      {socials.map(({ path, icon}, index) => (
+        <Link
+          key={index}
+          href={path}
+          target='_blank'
+          rel='noreferrer nofollow noopener'
+          className="hover:text-accent hover:scale-110 transition-all duration-300"
+        >
           {icon}
         </Link>
       ))}
