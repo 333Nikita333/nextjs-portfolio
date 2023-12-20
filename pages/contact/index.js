b945091d9aa153cb7e38a94fe1501cb5b64bbfda
333Nikita333 <nikitosfox333@gmail.com>
2023-12-20 14:35:30 +0200

    added logic for sending a message to the Contact page and some styling

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -2,8 +2,57 @@ import { motion } from 'framer-motion';
 import { BsArrowRight } from 'react-icons/bs';
 import { AnimatedText, Circles, Meta } from '../../components';
 import { fadeIn } from '../../variants';
+import { useState } from 'react';
+
+const initialData = {
+  name: '',
+  email: '',
+  subject: '',
+  message: '',
+};
 
 const Contact = () => {
+  const [formData, setFormData] = useState(initialData);
+
+  const handleChange = e => {
+    const { name, value } = e.target;
+    setFormData(prevData => ({
+      ...prevData,
+      [name]: value,
+    }));
+    console.log(formData)
+  };
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch('/api/contact', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+
+      const result = await response.json();
+      if (result.success) {
+        console.log('Message sent successfully!');
+        // Очистите форму после успешной отправки, если это необходимо
+        setFormData({
+          name: '',
+          email: '',
+          subject: '',
+          message: '',
+        });
+      } else {
+        console.error('Failed to send message.');
+      }
+    } catch (error) {
+      console.error('Error sending message:', error);
+    }
+  };
+
   return (
     <>
       <Meta
@@ -16,10 +65,10 @@ const Contact = () => {
         <Circles />
         <div
           className="container mx-auto py-32 text-center xl:text-left flex items-center justify-center 
-        h-full"
+        h-full max-xl:overflow-y-auto"
         >
           {/* text & form */}
-          <div className="flex flex-col w-full max-w-[700px]">
+          <div className="flex flex-col w-full max-w-[700px] max-xl:mt-auto">
             {/* text */}
             <AnimatedText
               isTypingAnimate={false}
@@ -36,26 +85,47 @@ const Contact = () => {
               animate="show"
               exit="hidden"
               className="flex-1 flex flex-col items-center gap-6 w-full mx-auto"
+              onSubmit={handleSubmit}
             >
               {/* group */}
               <div className="flex gap-x-6 w-full">
                 <input
-                  type="name"
+                  type="text"
                   placeholder="What is your name?"
-                  className="input"
+                  className="input max-sm:text-sm"
+                  name="name"
+                  value={formData.name}
+                  onChange={handleChange}
                 />
                 <input
                   type="email"
                   placeholder="What is your email?"
-                  className="input"
+                  className="input max-sm:text-sm"
+                  name="email"
+                  value={formData.email}
+                  onChange={handleChange}
                 />
               </div>
-              <input type="text" placeholder="subject" className="input" />
-              <textarea placeholder="message" className="textarea"></textarea>
+              <input
+                type="text"
+                placeholder="Message subject"
+                className="input max-sm:text-sm"
+                name="subject"
+                value={formData.subject}
+                onChange={handleChange}
+              />
+              <textarea
+                placeholder="Write your message"
+                className="textarea max-sm:text-sm"
+                name="message"
+                value={formData.message}
+                onChange={handleChange}
+              ></textarea>
               <button
                 className="btn rounded-full border border-white/50 max-w-[170px] px-8 
               transition-all duration-300 flex items-center justify-center overflow-hidden 
             hover:border-accent group"
+                type="submit"
               >
                 <span
                   className="group-hover:-translate-y-[120%] group-hover:opacity-0 
