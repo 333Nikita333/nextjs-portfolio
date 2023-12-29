d12eb1d58074da6c7f88f02bbd9580165e82d616
333Nikita333 <nikitosfox333@gmail.com>
2023-12-29 19:12:49 +0200

    try fix deploy on versel 21

--- pages/projects/[id].js
+++ pages/projects/[id].js
@@ -205,14 +205,19 @@ export default function Project({ project }) {
       title: 'Responsibilities',
       content: project?.responsibilities?.role ? (
         <div>
-          <p className="text-white-700">{project.responsibilities.role}</p>
-          <ul className="list-disc">
-            {project.responsibilities.list?.map((responsibility, index) => (
-              <li key={index} className="mb-2">
-                {responsibility}
-              </li>
-            ))}
-          </ul>
+          {project.responsibilities.role && (
+            <p className="text-white-700">{project.responsibilities.role}</p>
+          )}
+          {project.responsibilities.list &&
+            project.responsibilities.list.length > 0 && (
+              <ul className="list-disc">
+                {project.responsibilities.list.map((responsibility, index) => (
+                  <li key={index} className="mb-2">
+                    {responsibility}
+                  </li>
+                ))}
+              </ul>
+            )}
         </div>
       ) : (
         <p className="text-white-700">
