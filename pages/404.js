051c8a53352d5758636d94e340694adfc9b4e1c9
333Nikita333 <nikitosfox333@gmail.com>
2024-01-18 14:10:30 +0200

    added translation for 404 page

--- pages/404.js
+++ pages/404.js
@@ -1,25 +1,32 @@
 import Link from 'next/link';
 import { Meta } from '../components';
+import notFoundEn from '../locales/en/404.json';
+import notFoundUk from '../locales/uk/404.json';
+import { useRouter } from 'next/router';
 
 const NotFound = () => {
+  const router = useRouter();
+
+  const t = router.locale === 'en' ? notFoundEn : notFoundUk;
+
   return (
     <>
       <Meta
-        title="404 - Page not found"
-        description="The requested page is not found. Explore Mykyta's portfolio or return to the homepage."
+        title={t.metaTitle}
+        description={t.metaDescription}
         robots="noindex, nofollow"
       />
 
       <div className="h-full bg-primary/30 text-center flex flex-col items-center justify-center">
         <h1 className="text-6xl font-bold text-accent mb-4">404</h1>
         <p className="text-3xl mb-4 border border-accent border-dashed rounded-3xl p-4">
-          Page not found
+          {t.descriptionText}
         </p>
         <Link
           href="/"
           className="text-xl text-accent hover:scale-110 transition-all duration-300"
         >
-          Go to home
+          {t.buttonText}
         </Link>
       </div>
     </>
