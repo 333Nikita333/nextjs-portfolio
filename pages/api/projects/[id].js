a4a8d46622957294b140e941528f31be4ad50e6e
Mykyta Hilis <110134863+333Nikita333@users.noreply.github.com>
2024-01-19 20:03:27 +0200

    Update [id].js

--- pages/api/projects/[id].js
+++ pages/api/projects/[id].js
@@ -5,21 +5,26 @@ export default function handler(req, res) {
   const { id, locale } = req.query;
 
   if (req.method === 'GET') {
-    console.log('locale =>', locale);
     let project;
-
+    
     if (locale === 'en') {
       project = projectsDataEn.find(project => project.id === id);
+
+      if (project) {
+        res.status(200).json(project);
+      } else {
+        res.status(404).json({ message: 'Project not found' });
+      }
     }
 
     if (locale === 'uk') {
       project = projectsDataUk.find(project => project.id === id);
-    }
 
-    if (project) {
-      res.status(200).json(project);
-    } else {
-      res.status(404).json({ message: 'Project not found' });
+      if (project) {
+        res.status(200).json(project);
+      } else {
+        res.status(404).json({ message: 'Project not found' });
+      }
     }
   } else {
     res.status(405).json({ message: 'Method Not Allowed' });
